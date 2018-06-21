04ac693808ac782423c574c3320a0a2ec074645f
Michael Boreham <bor3ham@gmail.com>
2018-06-21 15:21:35 +1000

    Add bar render mode for process map

--- src/layout/process-map.js
+++ src/layout/process-map.js
@@ -1,29 +1,6 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import classNames from 'classnames'
-
-class ProcessMap extends React.Component {
-  render() {
-    let children = this.props.children
-    if (!Array.isArray(children)) {
-      children = [children]
-    }
-    let hitActive = false
-    return (
-      <div className="qbo-process-map">
-        {this.props.children.map((child, childIndex) => {
-          if (child.props.active) {
-            hitActive = true
-          }
-          return React.cloneElement(child, {
-            key: `item-${childIndex}`,
-            index: childIndex + 1,
-            previous: (!child.props.active && !hitActive),
-          })
-        })}
-      </div>
-    )
-  }
-}
 
 class ProcessItem extends React.Component {
   render() {
@@ -37,6 +14,42 @@ class ProcessItem extends React.Component {
       </div>
     )
   }
+}
+
+class ProcessMap extends React.Component {
+  render() {
+    let children = this.props.children
+    if (!Array.isArray(children)) {
+      children = [children]
+    }
+    let hitActive = false
+    let activeIndex = 0
+    children = this.props.children.map((child, childIndex) => {
+      if (child.props.active) {
+        hitActive = true
+        activeIndex = childIndex
+      }
+      return React.cloneElement(child, {
+        key: `item-${childIndex}`,
+        index: childIndex + 1,
+        previous: (!child.props.active && !hitActive),
+      })
+    })
+    return (
+      <div className={classNames('qbo-process-map', this.props.mode)}>
+        <div className="summary">Step {activeIndex + 1} of {children.length}</div>
+        <div className="children">
+          {children}
+        </div>
+      </div>
+    )
+  }
+}
+ProcessMap.defaultProps = {
+  mode: 'chapters',
+}
+ProcessMap.propTypes = {
+  mode: PropTypes.oneOf(['chapters', 'bar']),
 }
 
 export { ProcessMap, ProcessItem }
